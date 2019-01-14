97c68614d2a35b8a13b6fbcdeb3d4ec326342f76
Valeria Gora <valeriia308@gmail.com>
2019-01-15 00:29:23 +0300

    formatting and game checking changed

--- app.js
+++ app.js
@@ -1,153 +1,168 @@
-'use strict';
+"use strict";
 //Классы:
 //fas fa-times - крестики
 //far fa-circle - нолики
 
-let restart = document.querySelector('#restart-btn');
+let restart = document.querySelector("#restart-btn");
 
-let field = document.querySelector('#field');
+let field = document.querySelector("#field");
 
-field.addEventListener('click', drawCross);
-
+field.addEventListener("click", drawCross);
 
 function drawCross(e) {
-
   let target = e.target;
   let emptyCellsAmount;
 
-  if ( !target.closest('.field__cell') || !field.contains(target) ) {
+  if (!target.closest(".field__cell") || !field.contains(target)) {
     return;
   }
 
-  let trueTarget = target.closest('.field__cell');
-  if ( trueTarget.querySelector('i').classList.contains('fas') || trueTarget.querySelector('i').classList.contains('far') ) {
+  let trueTarget = target.closest(".field__cell");
+  if (
+    trueTarget.querySelector("i").classList.contains("fas") ||
+    trueTarget.querySelector("i").classList.contains("far")
+  ) {
     return;
   }
-  trueTarget.querySelector('i').classList.add('fas', 'fa-times');    
+  trueTarget.querySelector("i").classList.add("fas", "fa-times");
 
-  field.removeEventListener('click', drawCross);
+  field.removeEventListener("click", drawCross);
 
   emptyCellsAmount = cells.filter(function(cell) {
-    return cell.querySelector('i').classList.length === 0;
+    return cell.querySelector("i").classList.length === 0;
   }).length;
 
   let winnerIsDetermined = determineTheWinner();
-  if ( winnerIsDetermined ) {
+  if (winnerIsDetermined) {
     emptyCellsAmount = false;
     return;
   }
 
-  if ( emptyCellsAmount ) {
+  if (emptyCellsAmount) {
     let randomTd = findRandomCell();
-    randomTd.addEventListener('click', drawCircle);
+    randomTd.addEventListener("click", drawCircle);
     randomTd.click();
-    randomTd.removeEventListener('click', drawCircle);
-    field.addEventListener('click', drawCross);
+    randomTd.removeEventListener("click", drawCircle);
+    field.addEventListener("click", drawCross);
   }
-        
+
   winnerIsDetermined = determineTheWinner();
-  if ( winnerIsDetermined ) {
-    field.removeEventListener('click', drawCross);
+  if (winnerIsDetermined) {
+    field.removeEventListener("click", drawCross);
   } else {
-    if ( !emptyCellsAmount ) {
-      alert('Draw');
+    if (!emptyCellsAmount) {
+      resultField.textContent = "draw";
     }
   }
 }
 
 function drawCircle(e) {
   let target = e.target;
-  target.querySelector('i').classList.add('far', 'fa-circle'); 
+  target.querySelector("i").classList.add("far", "fa-circle");
 }
 
-let cells = Array.from( document.querySelectorAll('td') );
+let cells = Array.from(document.querySelectorAll("td"));
 
 function findRandomCell() {
-  let random = cells[Math.floor( Math.random() * cells.length )];
+  let random = cells[Math.floor(Math.random() * cells.length)];
   let i = random.children[0];
 
-  if ( i.classList.contains('fas') || i.classList.contains('far') ) {
+  if (i.classList.contains("fas") || i.classList.contains("far")) {
     return findRandomCell();
   } else {
     return random;
-  }  
+  }
 }
+
+let resultField = document.querySelector("#result-field");
 
 function determineTheWinner() {
   //КРЕСТИКИ
-  if ( cells[0].firstElementChild.classList.contains('fas') && cells[1].firstElementChild.classList.contains('fas') && cells[2].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[3].firstElementChild.classList.contains('fas') && cells[4].firstElementChild.classList.contains('fas') && cells[5].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[6].firstElementChild.classList.contains('fas') && cells[7].firstElementChild.classList.contains('fas') && cells[8].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[0].firstElementChild.classList.contains('fas') && cells[3].firstElementChild.classList.contains('fas') && cells[6].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[1].firstElementChild.classList.contains('fas') && cells[4].firstElementChild.classList.contains('fas') && cells[7].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[2].firstElementChild.classList.contains('fas') && cells[5].firstElementChild.classList.contains('fas') && cells[8].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[0].firstElementChild.classList.contains('fas') && cells[4].firstElementChild.classList.contains('fas') && cells[8].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
-  }
-  if ( cells[2].firstElementChild.classList.contains('fas') && cells[4].firstElementChild.classList.contains('fas') && cells[6].firstElementChild.classList.contains('fas')) {
-    alert('Congratulations! You win!');
-    return true;
+  for (let i = 0; i < cells.length; i++) {
+    if (i === 0 || i === 3 || i === 6) {
+      if (
+        cells[i].querySelector("i").classList.contains("fas") &&
+        cells[i + 1].querySelector("i").classList.contains("fas") &&
+        cells[i + 2].querySelector("i").classList.contains("fas")
+      ) {
+        resultField.textContent = "You win";
+        return true;
+      }
+    }
+    if (i === 0 || i === 1 || i === 2) {
+      if (
+        cells[i].querySelector("i").classList.contains("fas") &&
+        cells[i + 3].querySelector("i").classList.contains("fas") &&
+        cells[i + 6].querySelector("i").classList.contains("fas")
+      ) {
+        resultField.textContent = "You win";
+        return true;
+      }
+    }
+    if (
+      cells[0].querySelector("i").classList.contains("fas") &&
+      cells[4].querySelector("i").classList.contains("fas") &&
+      cells[8].querySelector("i").classList.contains("fas")
+    ) {
+      resultField.textContent = "You win";
+      return true;
+    }
+    if (
+      cells[2].querySelector("i").classList.contains("fas") &&
+      cells[4].querySelector("i").classList.contains("fas") &&
+      cells[6].querySelector("i").classList.contains("fas")
+    ) {
+      resultField.textContent = "You win";
+      return true;
+    }
   }
   //НОЛИКИ
-  if ( cells[0].firstElementChild.classList.contains('far') && cells[1].firstElementChild.classList.contains('far') && cells[2].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[3].firstElementChild.classList.contains('far') && cells[4].firstElementChild.classList.contains('far') && cells[5].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[6].firstElementChild.classList.contains('far') && cells[7].firstElementChild.classList.contains('far') && cells[8].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[0].firstElementChild.classList.contains('far') && cells[3].firstElementChild.classList.contains('far') && cells[6].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[1].firstElementChild.classList.contains('far') && cells[4].firstElementChild.classList.contains('far') && cells[7].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[2].firstElementChild.classList.contains('far') && cells[5].firstElementChild.classList.contains('far') && cells[8].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[0].firstElementChild.classList.contains('far') && cells[4].firstElementChild.classList.contains('far') && cells[8].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
-  }
-  if ( cells[2].firstElementChild.classList.contains('far') && cells[4].firstElementChild.classList.contains('far') && cells[6].firstElementChild.classList.contains('far')) {
-    alert('Sorry :(\n Computer wins');
-    return true;
+  for (let i = 0; i < cells.length; i++) {
+    if (i === 0 || i === 3 || i === 6) {
+      if (
+        cells[i].querySelector("i").classList.contains("far") &&
+        cells[i + 1].querySelector("i").classList.contains("far") &&
+        cells[i + 2].querySelector("i").classList.contains("far")
+      ) {
+        resultField.textContent = "Computer wins";
+        return true;
+      }
+    }
+    if (i === 0 || i === 1 || i === 2) {
+      if (
+        cells[i].querySelector("i").classList.contains("far") &&
+        cells[i + 3].querySelector("i").classList.contains("far") &&
+        cells[i + 6].querySelector("i").classList.contains("far")
+      ) {
+        resultField.textContent = "Computer wins";
+        return true;
+      }
+    }
+    if (
+      cells[0].querySelector("i").classList.contains("far") &&
+      cells[4].querySelector("i").classList.contains("far") &&
+      cells[8].querySelector("i").classList.contains("far")
+    ) {
+      resultField.textContent = "Computer wins";
+      return true;
+    }
+    if (
+      cells[2].querySelector("i").classList.contains("far") &&
+      cells[4].querySelector("i").classList.contains("far") &&
+      cells[6].querySelector("i").classList.contains("far")
+    ) {
+      resultField.textContent = "Computer wins";
+      return true;
+    }
   }
 
-  return false;   
+  return false;
 }
 
-restart.addEventListener('click', cleanTheField);
+restart.addEventListener("click", cleanTheField);
 
 function cleanTheField() {
-  cells.forEach((cell) => cell.firstElementChild.classList = '' );
-
-  field.addEventListener('click', drawCross);
+  cells.forEach(cell => (cell.firstElementChild.classList = ""));
+  resultField.textContent = "";
+  field.addEventListener("click", drawCross);
 }
